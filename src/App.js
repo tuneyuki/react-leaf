15dbac998d460d289051779ba1d98d9e9dbac175
$tu-itou-deployTest01 <tuneyuki@gmail.com>
2023-08-23 21:00:53 +0900

    React18 + Leaflet

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Map from './components/map';
+
+
+function App() {
+  return (
+    <div className='bg-gray-500 w-screen h-screen'>
+      <header className='h-16 bg-cyan-100 border-4'>
+        <div className='text-2xl bold'>
+          Title
+        </div>
+      </header>
+      <div id='Main'>
+        <div className='h-96 bg-emerald-200 flex w-full'>
+          <div className='w-[40%]'>
+            <div className='h-8'>ボタン</div>
+            <div className='h-auto'>
+              <Map />
+            </div>
+          </div>
+          <div className='w-[40%] border-2'>表</div>
+          <div className='w-[20%] border-2'>時計</div>
+        </div>
+        <div></div>
+      </div>
+
+    </div>
+      
+  );
+}
+
+export default App;
